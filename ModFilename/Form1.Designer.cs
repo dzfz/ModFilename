8d2da61052673af70a9a954317596dc06196e9ab
定之方中 <kgdpgk@qq.com>
2016-02-16 14:44:50 +0800

    init

---
+++ ModFilename/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace ModFilename
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_filePath = new System.Windows.Forms.TextBox();
+            this.button_selectPath = new System.Windows.Forms.Button();
+            this.button_run = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_filePath
+            // 
+            this.textBox_filePath.Location = new System.Drawing.Point(12, 12);
+            this.textBox_filePath.Name = "textBox_filePath";
+            this.textBox_filePath.Size = new System.Drawing.Size(162, 21);
+            this.textBox_filePath.TabIndex = 0;
+            // 
+            // button_selectPath
+            // 
+            this.button_selectPath.Location = new System.Drawing.Point(197, 13);
+            this.button_selectPath.Name = "button_selectPath";
+            this.button_selectPath.Size = new System.Drawing.Size(75, 23);
+            this.button_selectPath.TabIndex = 1;
+            this.button_selectPath.Text = "目录";
+            this.button_selectPath.UseVisualStyleBackColor = true;
+            this.button_selectPath.Click += new System.EventHandler(this.button_selectPath_Click);
+            // 
+            // button_run
+            // 
+            this.button_run.Location = new System.Drawing.Point(99, 76);
+            this.button_run.Name = "button_run";
+            this.button_run.Size = new System.Drawing.Size(75, 23);
+            this.button_run.TabIndex = 2;
+            this.button_run.Text = "开始";
+            this.button_run.UseVisualStyleBackColor = true;
+            this.button_run.Click += new System.EventHandler(this.button_run_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button_run);
+            this.Controls.Add(this.button_selectPath);
+            this.Controls.Add(this.textBox_filePath);
+            this.Name = "Form1";
+            this.Text = "格式化文件名";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_filePath;
+        private System.Windows.Forms.Button button_selectPath;
+        private System.Windows.Forms.Button button_run;
+    }
+}
+
---
+++ ModFilename/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace ModFilename
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_run_Click(object sender, EventArgs e)
+        {
+            string filePath = this.textBox_filePath.Text;
+            if (filePath != "" && filePath != null)
+            {
+                string[] filenames = Directory.GetDirectories(filePath);
+                if (filenames.Length > 0)
+                {
+                    foreach (string pathName in filenames)
+                    {
+                        DirectoryInfo info = new DirectoryInfo(pathName);
+                        if (info != null)
+                        {
+                            string name = info.Name;
+                            string newName = Regex.Replace(name, @"([\S\s]+?)[ ]{1,}([\d]{4}-[\d]+-[\d]+)[ ]{1,}([\s\S]+)", @"$2 $1 $3");
+                            //MessageBox.Show(filePath+"\\"+newName);
+                            if (newName != "" && newName != null && !name.Equals(newName))
+                            {
+                                try
+                                {
+                                    info.MoveTo(filePath + @"\" + newName);
+                                }
+                                catch (IOException ioe)
+                                {
+                                    MessageBox.Show(ioe.Message);
+                                }
+
+                            }
+
+                        }
+                        //MessageBox.Show(name);
+                        //Directory.Move()
+                    }
+                    MessageBox.Show("重命名完成！");
+                }
+                else
+                {
+                    MessageBox.Show("该目录中没有文件夹！");
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("文件目录不能为空！");
+            }
+
+        }
+
+        private void button_selectPath_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            this.textBox_filePath.Text = fbd.SelectedPath;
+        }
+    }
+}
